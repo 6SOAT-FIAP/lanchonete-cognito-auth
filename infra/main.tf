a7d02d22df0dae445be052764d524268c79d1a1b
DeividCezar <perspicilum@gmail.com>
2024-09-26 00:19:25 -0300

    Create user pool clientes

---
+++ infra/main.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_cognito_user_pool" "clientes_lanchonete_user_pool" {
+  name              = var.user_pool_name
+  mfa_configuration = var.mfa_configuration
+
+  schema {
+    name                = "name"
+    attribute_data_type = "String"
+    required            = true
+  }
+
+  schema {
+    name                = "email"
+    attribute_data_type = "String"
+    required            = true
+  }
+
+  lambda_config {
+    pre_sign_up                    = aws_lambda_function.lanchonete_lambda_auth.arn:aws:lambda:us-east-1:105971623004:function:lanchonete-lambda-pre-sign-up
+    define_auth_challenge          = aws_lambda_function.lanchonete_lambda_auth.arn:aws:lambda:us-east-1:105971623004:function:lanchonete-lambda-define-auth-challenge
+    create_auth_challenge          = aws_lambda_function.lanchonete_lambda_auth.arn:aws:lambda:us-east-1:105971623004:function:lanchonete-lambda-create-auth-challenge
+    verify_auth_challenge_response = aws_lambda_function.lanchonete_lambda_auth.arn:aws:lambda:us-east-1:105971623004:function:lanchonete-lambda-verify-auth-challenge
+  }
+
+  password_policy {
+    minimum_length    = 8
+    require_uppercase = true
+    require_lowercase = true
+    require_numbers   = true
+    require_symbols   = true
+  }
+}
+
+resource "aws_cognito_user_pool_client" "lanchonete_user_pool_client" {
+  name            = "lanchonete-client"
+  user_pool_id    = aws_cognito_user_pool.clientes_lanchonete_user_pool.id
+  generate_secret = false
+}
