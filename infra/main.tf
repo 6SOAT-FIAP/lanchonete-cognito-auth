e64e1340094ade6e0878bf1f6350c269e9a66c31
DeividCezar <perspicilum@gmail.com>
2024-09-27 01:25:34 -0300

    Ajuste para criação única

--- infra/main.tf
+++ infra/main.tf
@@ -40,6 +40,38 @@ resource "aws_cognito_user_pool_client" "lanchonete_user_pool_client" {
   generate_secret = false
 }
 
+resource "aws_lambda_permission" "allow_cognito_invoke_pre_sign_up" {
+  statement_id  = "AllowExecutionFromCognitoPreSignUp"
+  action        = "lambda:InvokeFunction"
+  function_name = "lanchonete-lambda-pre-sign-up"
+  principal     = "cognito-idp.amazonaws.com"
+  source_arn    = aws_cognito_user_pool.clientes_lanchonete_user_pool.arn
+}
+
+resource "aws_lambda_permission" "allow_cognito_invoke_define_auth_challenge" {
+  statement_id  = "AllowExecutionFromCognitoDefineAuthChallenge"
+  action        = "lambda:InvokeFunction"
+  function_name = "lanchonete-lambda-define-auth-challenge"
+  principal     = "cognito-idp.amazonaws.com"
+  source_arn    = aws_cognito_user_pool.clientes_lanchonete_user_pool.arn
+}
+
+resource "aws_lambda_permission" "allow_cognito_invoke_create_auth_challenge" {
+  statement_id  = "AllowExecutionFromCognitoCreateAuthChallenge"
+  action        = "lambda:InvokeFunction"
+  function_name = "lanchonete-lambda-create-auth-challenge"
+  principal     = "cognito-idp.amazonaws.com"
+  source_arn    = aws_cognito_user_pool.clientes_lanchonete_user_pool.arn
+}
+
+resource "aws_lambda_permission" "allow_cognito_invoke_verify_auth_challenge_response" {
+  statement_id  = "AllowExecutionFromCognitoVerifyAuthChallengeResponse"
+  action        = "lambda:InvokeFunction"
+  function_name = "lanchonete-lambda-verify-auth-challenge"
+  principal     = "cognito-idp.amazonaws.com"
+  source_arn    = aws_cognito_user_pool.clientes_lanchonete_user_pool.arn
+}
+
 resource "aws_cognito_user" "cliente_balcao" {
   user_pool_id = aws_cognito_user_pool.clientes_lanchonete_user_pool.id
   username     = "99999999999"
@@ -49,7 +81,5 @@ resource "aws_cognito_user" "cliente_balcao" {
     email = "balcao@gmail.com"
   }
 
-  lifecycle {
-    create_before_destroy = true
-  }
+  depends_on = [aws_lambda_permission.allow_cognito_invoke_pre_sign_up]
 }
